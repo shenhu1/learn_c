b4c9f5df8a40666b0c4a5bc7857dafc3b470e3fc
shenhu <2287169399@qq.com>
2017-11-05 21:02:42 +0800

    scanf,printf usage

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 
 int main(int argc,char* argv[])
 {
-   printf("hello world");
-   return 0;
+    char name[10];
+    printf("Input your name: ");
+    scanf("%s",name);
+    printf("Your name is: %s",name);
+    return 0;
 }
